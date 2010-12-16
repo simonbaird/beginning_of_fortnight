1a728c838a1e5d2c8224277883be9d1bce63b04f
Simon Baird <sbaird@monicit.com.au>
2010-12-17 09:44:26 +1000

    Borked it someone using require in the gem. Dunno what's going on exactly.

--- lib/beginning_of_fortnight.rb
+++ lib/beginning_of_fortnight.rb
@@ -3,9 +3,8 @@
 # end_of_fortnight method for the Time class
 #
 #
-
-# Sanity check?
-raise "Can't find suitable ActiveSupport module" unless defined?(ActiveSupport::CoreExtensions::Time::Calculations)
+#
+require 'active_support/time'
 
 class BeginningOfFortnight
   #
@@ -25,31 +24,38 @@ class BeginningOfFortnight
 
 end
 
-module ActiveSupport::CoreExtensions::Time::Calculations
-  #
-  # The beginning of the current fortnight
-  #
-  def beginning_of_fortnight
-    # How many weeks since the beginning_of_week before unix time zero
-    # (We just need a reference date. Probably any arbitrary time would work just as well).
-    weeks_since_reference = ((self - BeginningOfFortnight.reference_date.beginning_of_week) / 1.week).to_i
+module ActiveSupport #nodoc#
+  module CoreExtensions #nodoc#
+    module Time #nodoc#
+      module Calculations #nodoc#
 
-    # When there's even number of weeks since epoch use beginning of this week,
-    # odd number use beginning of last week
-    #
-    # The choice of which week should begin a fortnight is arbitrary (as far as I know)
-    # If we change even to odd below then it would be flipped.
-    #
-    # Does it work for negative weeks_since_reference? Better write some tests.. :)
-    #
-    (weeks_since_reference.even? ? self : self - 1.week).beginning_of_week
+        #
+        # The beginning of the current fortnight
+        #
+        def beginning_of_fortnight
+          # How many weeks since the beginning_of_week before unix time zero
+          # (We just need a reference date. Probably any arbitrary time would work just as well).
+          weeks_since_reference = ((self - BeginningOfFortnight.reference_date.beginning_of_week) / 1.week).to_i
+
+          # When there's even number of weeks since epoch use beginning of this week,
+          # odd number use beginning of last week
+          #
+          # The choice of which week should begin a fortnight is arbitrary (as far as I know)
+          # If we change even to odd below then it would be flipped.
+          #
+          # Does it work for negative weeks_since_reference? Better write some tests.. :)
+          #
+          (weeks_since_reference.even? ? self : self - 1.week).beginning_of_week
+        end
+
+        #
+        # The end of the current fortnight can be easily derived from the beginning like this
+        #
+        def end_of_fortnight
+          (beginning_of_fortnight + 13.days).end_of_day
+        end
+
+      end
+    end
   end
-
-  #
-  # The end of the current fortnight can be easily derived from the beginning like this
-  #
-  def end_of_fortnight
-    (beginning_of_fortnight + 13.days).end_of_day
-  end
-
 end
--- test.rb
+++ test.rb
@@ -1,9 +1,11 @@
 
 require 'rubygems'
-require 'active_support'
 require 'beginning_of_fortnight'
 
 now = Time.now
+
+puts now.beginning_of_week
+puts now.end_of_week
 
 puts now.beginning_of_fortnight
 puts now.end_of_fortnight
